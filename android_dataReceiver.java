1562db32accc79ced3b36707adb5b352d57ee2cb
giovaboy <giovaboy@users.noreply.github.com>
2015-11-10 21:41:49 +0100

    Update android_dataReceiver.java

--- android_dataReceiver.java
+++ android_dataReceiver.java
@@ -1,21 +1,20 @@
 package com.giovannizilli.pebblestrava.app;
 
-import com.getpebble.android.kit.PebbleKit;
-import com.getpebble.android.kit.util.PebbleDictionary;
-
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
-import android.util.Log;
+
+import com.getpebble.android.kit.PebbleKit;
+import com.getpebble.android.kit.util.PebbleDictionary;
 
 import java.util.UUID;
 
 public class DataReceiver extends com.getpebble.android.kit.PebbleKit.PebbleDataReceiver {
-    private static final int STRAVA_TOGGLE = 5;
-    private static final int STRAVA_START = 1;
+    //private static final int STRAVA_TOGGLE = 5;
+    private static final int STRAVA_START_RUN = 3;
+    private static final int STRAVA_START_RIDE = 1;
     private static final int STRAVA_STOP = 2;
 
-    private static final String TAG = "PB-PebbleDataReceiver";
     private static final UUID MYSTRAVA_PEBBLE_APP_UUID = UUID.fromString("45bd95bf-0b73-4e94-b03e-dca552f24d25");
 
     public DataReceiver() {
